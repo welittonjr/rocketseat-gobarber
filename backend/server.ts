3875829486c88168fea3dc7ea43777a0ab6dce7b
welittonjr <wellington.junior@madeiramadeira.com.br>
2021-11-26 09:33:56 -0300

    Update server

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-// import routes from './src/routes';
+import routes from './src/routes';
 
-// import './src/database';
+import './src/database';
 
 const app = express();
 
 app.use(express.json());
-// app.use(routes);
+app.use(routes);
 
 app.listen(3333, () => {
   console.log(' Server started on port 3333!');
