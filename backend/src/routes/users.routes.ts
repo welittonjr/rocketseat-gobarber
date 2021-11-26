609239cffdbecdded8c8aa5ff8f506b2af06f11d
welittonjr <wellington.junior@madeiramadeira.com.br>
2021-11-26 09:33:35 -0300

    rota de usuario

---
+++ backend/src/routes/users.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import UserService from "../services/UserService";
+
+const UsersRouter = Router();
+
+UsersRouter.post("/", async (request, response) => {
+    try {
+        const { name, email, password } = request.body;
+        
+        const userService = new UserService();
+
+        const user = userService.store({ 
+            name, 
+            email, 
+            password
+        });
+
+        return response.json(user);
+
+    } catch (err) {
+        return response.status(400).json({ error: err });
+    }
+});
+
+export default UsersRouter;
