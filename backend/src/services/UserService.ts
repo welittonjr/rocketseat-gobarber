ee0110dcededd5632f61fe60510607e4f22dc839
welittonjr <wellington.junior@madeiramadeira.com.br>
2021-11-26 09:15:51 -0300

    criado o serviço de usuarios

---
+++ backend/src/services/UserService.ts
@@ -0,0 +1,33 @@
+import { getRepository } from "typeorm";
+import User from "../entity/User"
+interface Request {
+    name: string;
+    email: string;
+    password: string;
+}
+class UserService {
+
+    public async store({ name, email, password }: Request): Promise<User> {
+        const usersRepository = getRepository(User);
+
+        const checkUserExists = await usersRepository.findOne({
+            where: { email },
+        });
+
+        if (checkUserExists) {
+            throw new Error('Email address already used');
+        }
+
+        const user = usersRepository.create({
+            name,
+            email,
+            password,
+        });
+
+        await usersRepository.save(user);
+
+        return user;
+    }
+}
+
+export default UserService;
