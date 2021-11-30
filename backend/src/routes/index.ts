86f8687cc567c4d90474b408f3dff8da64ad638e
welittonjr <wellington.junior@madeiramadeira.com.br>
2021-11-30 18:53:34 -0300

    Update project

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,6 +3,10 @@ import usersRouter from './users.routes';
 
 const routes = Router();
 
+routes.use('/alive', (_, response) => {
+    return response.status(200).json({ message: "I\'m alive" })
+});
+
 routes.use('/users', usersRouter);
 
 export default routes;
