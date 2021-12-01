9fbd02d856d424594caf3b5ad85b94b4a85f91aa
welittonjr <wellington.junior@madeiramadeira.com.br>
2021-12-01 12:01:39 -0300

    adicionando password

--- backend/src/database/migrations/1637800351907-CreateUsers.ts
+++ backend/src/database/migrations/1637800351907-CreateUsers.ts
@@ -1,44 +1,51 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export default class CreateUsers1637800351907 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.createTable(
-        new Table({
-          name: 'users',
-          columns:[
-            {
-              name: 'id',
-              type: 'int',
-              isPrimary: true,
-            },
-            {
-              name: 'name',
-              type: 'varchar',
-              isNullable: false,
-            },
-            {
-              name: 'email',
-              type: 'varchar',
-              isUnique: true,
-            },
-            {
-              name: 'created_at',
-              type: 'timestamp',
-              default: 'now()',
-            },
-            {
-              name: 'updated_at',
-              type: 'timestamp',
-              default: 'now()',
-            },
-          ],
-        }),
-      );
+        await queryRunner.createTable(
+            new Table({
+                name: 'users',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'integer',
+                        isPrimary:true,
+                        isGenerated:true,
+                        generationStrategy:'increment'
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
+                        isUnique: true,
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                ],
+            }),
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.dropTable('users');
+        await queryRunner.dropTable('users');
     }
 
 }
