a478bc417059c4f93e97805bed38eabb3cc84726
welittonjr <wellington.junior@madeiramadeira.com.br>
2021-12-01 12:05:52 -0300

    Update project

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+import * as dotenv from "dotenv";
+import { Cors } from './src/middleware/cors';
 import routes from './src/routes';
-
 import './src/database';
+
+if (process.env.NODE_ENV == "dev")
+    dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(Cors)
 app.use(routes);
 
 app.listen(3000, () => {
