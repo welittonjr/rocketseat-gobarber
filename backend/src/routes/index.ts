90c546faebba0bd2a885dbc5d7f6b00a92d5907a
welittonjr <wellington.junior@madeiramadeira.com.br>
2021-12-15 09:22:32 -0300

    rota de agendamento

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,14 +1,16 @@
-import { Router } from 'express';
-import sessionsRouter from './sessions.routes';
-import usersRouter from './users.routes';
+import { Router } from 'express'
+import sessionsRouter from './sessions.routes'
+import usersRouter from './users.routes'
+import scheduleRouter from './schedules.routes'
 
-const routes = Router();
+const routes = Router()
 
 routes.use('/alive', (_, response) => {
-    return response.status(200).json({ message: "I\'m alive" })
-});
+  return response.status(200).json({ message: "I\'m alive" })
+})
 
-routes.use('/users', usersRouter);
-routes.use('/sessions', sessionsRouter);
+routes.use('/users', usersRouter)
+routes.use('/schedules', scheduleRouter)
+routes.use('/sessions', sessionsRouter)
 
-export default routes;
+export default routes
---
+++ backend/src/routes/schedules.routes.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express'
+import { parseISO } from 'date-fns'
+
+import SchedulesRepository from '../repositories/SchedulesRepository'
+import SchedulesService from '../services/SchedulesService'
+import { getCustomRepository } from 'typeorm'
+
+const schedulesRouter = Router()
+
+schedulesRouter.get('/', async (request, response) => {
+  const schedulesRepository = getCustomRepository(SchedulesRepository)
+  const schedules = await schedulesRepository.find()
+
+  return response.json(schedules)
+})
+
+schedulesRouter.post('/', async (request, response) => {
+  try {
+    const { provider, date } = request.body
+
+    const parsedDate = parseISO(date)
+
+    const scheduleService = new SchedulesService()
+
+    const schedule = await scheduleService.execute({
+      date: parsedDate,
+      provider
+    })
+
+    return response.json(schedule)
+  } catch (err) {
+    return response.status(400).json({ error: err })
+  }
+})
+
+export default schedulesRouter
