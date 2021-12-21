4547380142a272ff1dd0500217f1fae70ddb8879
welittonjr <wellington.junior@madeiramadeira.com.br>
2021-12-21 08:29:49 -0300

    rota statica para arquivos

--- backend/server.ts
+++ backend/server.ts
@@ -1,18 +1,21 @@
-import express from 'express';
-import * as dotenv from "dotenv";
-import { Cors } from './src/middleware/cors';
-import routes from './src/routes';
-import './src/database';
+import express from 'express'
+import * as dotenv from 'dotenv'
+import { Cors } from './src/middleware/cors'
+import routes from './src/routes'
+import './src/database'
+import uploadConfig from './src/config/upload'
 
-if (process.env.NODE_ENV == "dev")
-    dotenv.config();
+if (process.env.NODE_ENV === 'dev') {
+  dotenv.config()
+}
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
+app.use('/files', express.static(uploadConfig.directory))
 app.use(Cors)
-app.use(routes);
+app.use(routes)
 
 app.listen(3000, () => {
   console.log(' Server started on port 3000!');
-});
+})
