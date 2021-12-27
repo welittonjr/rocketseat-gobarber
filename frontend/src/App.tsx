642e074304d08bf6c4a68023b867999472cbca8a
welittonjr <wellington.junior@madeiramadeira.com.br>
2021-12-27 07:22:41 -0300

    component signin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import SignIn from './pages/SignIn'
 import GlobalStyles from './styles/global'
 import './App.css'
 
 const App = () => {
   return (
     <>
-      <h1>Hello World</h1>
+      <SignIn />
       <GlobalStyles />
     </>
   )
