2ff82b94673e82e38bc8118b8d51fe083a102da9
welittonjr <wellington.junior@madeiramadeira.com.br>
2021-12-27 08:52:25 -0300

    component input

---
+++ frontend/src/components/Input/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background: #232129;
+  border-radius: 10px;
+  border: 2px solid #232129;
+  padding: 16px;
+  width: 100%;
+  color: #666360;
+
+  input {
+    flex: 1;
+    background: transparent;
+    border: 0;
+
+    &::placeholder {
+      color: #666360;
+    }
+
+    & + input {
+      margin-top: 8px;
+    }
+  }
+
+  svg {
+    margin-right: 16px;
+  }
+`
