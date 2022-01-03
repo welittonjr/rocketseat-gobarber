2ce3bef182afc62843415831764ea46936c8cb42
welittonjr <wellington.junior@madeiramadeira.com.br>
2022-01-03 08:53:13 -0300

    adicionando cors

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
-import { Cors } from './src/middleware/cors'
+import cors from 'cors'
 import routes from './src/routes'
 import './src/database'
 import uploadConfig from './src/config/upload'
@@ -13,7 +13,7 @@ const app = express()
 
 app.use(express.json())
 app.use('/files', express.static(uploadConfig.directory))
-app.use(Cors)
+app.use(cors())
 app.use(routes)
 
 app.listen(3000, () => {
