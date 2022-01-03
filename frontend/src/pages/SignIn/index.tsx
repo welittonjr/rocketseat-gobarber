8cdfe99cd3c94ef16d42460ca4debdad93d3aaee
welittonjr <wellington.junior@madeiramadeira.com.br>
2022-01-03 08:46:31 -0300

    ajuste em login

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,52 +1,75 @@
-import React, { useState } from 'react'
+import { Form } from '@unform/web'
+import React, { useCallback, useRef } from 'react'
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 import logoImg from '../../assets/logo.svg'
 import Button from '../../components/Button'
 import Input from '../../components/Input'
-
+import * as Yup from 'yup'
 import { Container, Content, Background } from './styles'
+import { FormHandles } from '@unform/core'
+import getValidationsErrors from '../../utils/getValidationErrors'
+
+interface SignInFormData {
+  email: string;
+  password: string;
+}
 
 const SignIn: React.FC = () => {
 
-  const singInInitState = {
-    email: "",
-    password: ""
-  }
+  const formRef = useRef<FormHandles>(null);
 
-  const [ singin, setSingIn ] = useState(singInInitState)
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório!')
+            .email('Digite um e-mail válido!'),
+          password: Yup.string().min(6, 'Senha obrigatório!'),
+        });
 
-  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-    const { name, value } = e.target;
-    setSingIn({ ...singin, [name]: value });
-  }
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        formRef.current?.setErrors({
+          name: 'Nome obrigatório'
+        })
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationsErrors(err);
+          formRef.current?.setErrors(errors);
+        }
+      }
+    },
+    [],
+  );
 
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="GoBarber" />
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu logon</h1>
 
           <Input 
             name="email" 
             icon={FiMail} 
-            placeholder="E-mail" 
-            onChange={handleInputChange} />
+            placeholder="E-mail" />
 
           <Input 
             name="password" 
             icon={FiLock} 
             type="password" 
-            placeholder="Senha"
-            onChange={handleInputChange} />
+            placeholder="Senha" />
 
           <Button type="submit">Entrar</Button>
 
-          <a href="#">
+          <a href="/">
             <FiLogIn />
             Esqueci minha senha
           </a>
-        </form>
+        </Form>
       </Content>
       <Background />
     </Container>
