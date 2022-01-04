a735c72540290574efa925b02f48552ceff8945d
welittonjr <wellington.junior@madeiramadeira.com.br>
2022-01-04 09:19:23 -0300

    contexto de autenticaçao em login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import SignIn from './pages/SignIn'
 import GlobalStyles from './styles/global'
 import './App.css'
+import { AuthProvider } from './context/AuthContext'
 
 const App = () => {
   return (
     <>
-      <SignIn />
+      <AuthProvider>
+        <SignIn />
+      </AuthProvider>
       <GlobalStyles />
     </>
   )
