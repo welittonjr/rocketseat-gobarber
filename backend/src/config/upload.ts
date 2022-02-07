ed4702f223d9f09743520b4626d9842b26a47662
welittonjr <wellington.junior@madeiramadeira.com.br>
2022-02-07 08:45:46 -0300

    backend completo

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -1,18 +1,45 @@
-import path from 'path'
-import crypto from 'crypto'
-import multer from 'multer'
+import multer, { StorageEngine } from 'multer';
+import crypto from 'crypto';
+import path from 'path';
 
-const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp')
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+  tmpFolder: string;
+  uploadsFolder: string;
+  multer: {
+    storage: StorageEngine;
+  };
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
 
 export default {
-  directory: tmpFolder,
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename (request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex')
-      const fileName = `${fileHash}-${file.originalname}`
+  driver: process.env.STORAGE_DRIVER,
+  tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
-      return callback(null, fileName)
-    }
-  })
-}
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(req, file, cb) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+        const fileName = `${fileHash}-${file.originalname}`;
+
+        return cb(null, fileName);
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'app-gobarber',
+    },
+  },
+} as IUploadConfig;
--- backend/src/modules/users/services/AuthenticateUserService.ts
+++ backend/src/modules/users/services/AuthenticateUserService.ts
@@ -1,46 +1,58 @@
-import { getRepository } from "typeorm";
-import { compare } from 'bcryptjs';
+import { injectable, inject } from 'tsyringe';
 import { sign } from 'jsonwebtoken';
+import authConfig from '@config/auth';
 
-import User from '../entities/User';
+import AppError from '@shared/errors/AppError';
+import User from '@modules/users/infra/typeorm/entities/User';
 
-interface Request {
-    email: string;
-    password: string;
+import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+
+interface IRequest {
+  email: string;
+  password: string;
 }
 
-interface Response {
-    user: User;
-    token: String;
+interface IResponse {
+  user: User;
+  token: string;
 }
 
+@injectable()
 class AuthenticateUserService {
-    public async execute({ email, password }: Request): Promise<Response> {
-        const usersRepository = getRepository(User);        
-        
-        const user = await usersRepository.findOne({ where: { email } })
-        
-        if (!user) {
-            throw new Error('Incorrenct email/password combination.');
-        }
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
 
-        const passwordMatched = await compare(password, user.password);
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
 
-        if (!passwordMatched) {
-            throw new Error('Incorrenct email/password combination.');
-        }
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
 
-        const token = sign({}, 'hash_2021', {
-            subject: user.id.toString(),
-            expiresIn: '1d'
-        });
-        console.log(token);
-        return {
-            user,
-            token,
-        };
-        
+    if (!user) {
+      throw new AppError('Incorrect email/password combination.', 401);
     }
+
+    const passwordMatch = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
+
+    if (!passwordMatch) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return { user, token };
+  }
 }
 
 export default AuthenticateUserService;
